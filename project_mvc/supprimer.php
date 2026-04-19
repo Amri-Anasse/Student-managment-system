0bfdf0867a321579cc554970780d984f90fbc247
Amri-Anasse <amrianasse555@gmail.com>
2026-04-19 13:32:55 +0100

    Update supprimer.php

--- project_mvc/supprimer.php
+++ project_mvc/supprimer.php
@@ -3,7 +3,7 @@ require 'connexion.php';
 
 $id = $_GET['id'];
 
-$stmt = $pdo->prepare("DELETE FROM etudiant WHERE id=?");
+$stmt = $pdo->prepare("DELETE FROM etudiants WHERE id=?");
 $stmt->execute([$id]);
 
 header("Location: liste.php");
