7cea5ea803a081ce221d3ae3d2ebf5ba0a536ddd
Amri-Anasse <amrianasse555@gmail.com>
2026-04-19 13:13:04 +0100

    footer.php

--- project_mvc/includes/footer.php
+++ project_mvc/includes/footer.php
@@ -1,7 +1,7 @@
 </div>
 
 <footer class="text-center text-muted py-4 mt-5 border-top">
-    <p>&copy; 2025 – GestEtu | ILCS – Pr. RABHI Ouzayr</p>
+    <p>&copy; 2026-Student Managment System | Amri Anasse</p>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
